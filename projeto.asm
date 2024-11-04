d9a75c048c320c3c50f4fb691abc5378f50b0851
Kayky Pires <125900277+KaykyyPiress@users.noreply.github.com>
2024-11-04 18:29:40 -0300

    Versão final com motor certo

--- projeto.asm
+++ projeto.asm
@@ -5,6 +5,11 @@ EN      equ     P1.2
 org 0000h
 	LJMP START
 
+org 001Bh ; Vetor de interrupção do Timer 1
+    AJMP TIMER_TRUE ; Salta para a rotina de interrupção do Timer 1
+org 001Bh ; Vetor de interrupção do Timer 1
+    AJMP TIMER_FAILD ; Salta para a rotina de interrupção do Timer 1
+
 org 0030h
 START:
 	; 0 = 41h
@@ -19,9 +24,9 @@ START:
 	; 9 = 43h
 
 	MOV 61H, #43h ; 9  
-	MOV 62H, #45h ; 7
-	MOV 63H, #49h ; 3
-	MOV 64H, #4Bh ; 1
+	MOV 62H, #43h ; 7
+	MOV 63H, #43h ; 3
+	MOV 64H, #43h ; 1
 
 	MOV 40H, #'#' 
 	MOV 41H, #'0'
@@ -37,12 +42,13 @@ START:
 	MOV 4BH, #'1'
 	  
 	
-	MOV R5,#3
-	acall lcd_init
+	MOV R5,#3 ; numero de tentantivas
+	acall lcd_init; inicia o lcd
 MAIN:
-	MOV R1, #51H
-	MOV R3, #4
+	MOV R1, #51H ; move pro R1 o endereço 51h 
+	MOV R3, #4 ; numero de digitos da senha
 	
+	;;imprime a mensagem no lcd para digitar a senha
 	mov A, #00h
 	ACALL posicionaCursor 
 	MOV A, #'S'
@@ -84,44 +90,46 @@ MAIN:
 	MOV A, #07h
 	ACALL posicionaCursor
 
+
 ROTINA:
 	
 	ACALL leituraTeclado
 	JNB F0, ROTINA  
-		
+	
 	MOV A, #40h
 	ADD A, R0
 	MOV R0, A
 
-	MOV @R1, A
-	INC R1
+	MOV @R1, A ;move pro endereço do r1 o digito da senha
+	INC R1; incrementa o r1
 
 	MOV A, @R0  
 	    
 	ACALL sendCharacter
 	CLR F0
-	DJNZ R3, ROTINA
+	DJNZ R3, ROTINA ; decrementa o a quantidade de digitos
 	
-	MOV R1, #51H
-	MOV R0, #61H
-	MOV R4, #4
+	MOV R1, #51H; move pro r1 o endereço dos digitos da senha
+	MOV R0, #61H;  move pro r0 o endereço dos digitos da senha ja setada
+	MOV R4, #4; move pro r4 o numero de vezes que realizará a comparação
 
 	COMPARACAO:
 		
-		MOV A, @R0
-		MOV B, @R1
+		MOV A, @R0 ;move pro a o endereço do r0
+		MOV B, @R1; move pro b o endereço do r1
 		
-		CJNE A,B , DIFERENTE
-
-		INC R1
-		INC R0
+		CJNE A,B , DIFERENTE ;confere se os digitos estão iguais, se tiver iguais 
+							; ele continua a execução, se estiver diferente ele imprime a errada
+		INC R1 ; incrementa o r1
+		INC R0; incrementa o r0
 		
-		DJNZ R4, COMPARACAO
-		SJMP CORRETO
+		DJNZ R4, COMPARACAO ; decrementa o r4 para conferir os digitos com a senha
+		SJMP CORRETO; inicia a função de correto
 
 CORRETO:
-		MOV P1, #0FDH ;liga todos os led
-
+		MOV P1, #0FDH ;liga o led verde
+		
+		;imprime no lcd a mensagem de aberto 
 		mov A, #00h
 		ACALL posicionaCursor 
 		MOV A, #'_'
@@ -157,16 +165,28 @@ CORRETO:
 		MOV A, #'_'
 		ACALL sendCharacter
 
-		MOTORDC:
+		MOTORTRUE:
 			MOV TMOD, #10H
+			MOV TL1, #253
+			MOV TH1, #253
 			SETB EA
 			SETB ET1
 			SETB TR1
-			SETB P3.0
-			CLR P3.1
-		SJMP $
+			SETB P3.0 ; Liga o motor
+			CLR P3.1 ; Define direção do motor
+			SJMP $
+
+		; Rotina de interrupção do Timer 1 para parar o motor após uma rotação
+		TIMER_TRUE:
+			CLR TR1 ; Para o Timer 1
+			CLR P3.0 ; Desliga o motor
+			CLR ET1 ; Desativa interrupção do Timer 1
+			RETI ; Retorna da interrupção
+
+;caso a senha estiver incorreta executara essa função
 
 DIFERENTE:
+
 	mov A, #00h
 	ACALL posicionaCursor 
 	MOV A, #'_'
@@ -243,7 +263,25 @@ FIM:
 		ACALL sendCharacter
 		MOV A, #'_'
 		ACALL sendCharacter
-		SJMP $
+
+		MOTORFAILD:
+			MOV TMOD, #10H
+			MOV TL1, #252
+			MOV TH1, #252
+			SETB EA
+			SETB ET1
+			SETB TR1
+			SETB P3.0 ; Liga o motor
+			CLR P3.1 ; Define direção do motor
+			SJMP $
+
+		; Rotina de interrupção do Timer 1 para parar o motor após uma rotação
+		TIMER_FAILD:
+			CLR TR1 ; Para o Timer 1
+			CLR P3.0 ; Desliga o motor
+			CLR ET1 ; Desativa interrupção do Timer 1
+			RETI ; Retorna da interrupção
+
 leituraTeclado:
 	MOV R0, #0		
 	MOV P0, #0FFh	
